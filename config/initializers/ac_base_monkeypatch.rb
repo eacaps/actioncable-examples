9f0aaac0bf8a8867b2e0268206a7c226efb93b00
eacaps <eacaps@gmail.com>
2016-04-26 14:59:42 -0400

    fix debugging message

--- config/initializers/ac_base_monkeypatch.rb
+++ config/initializers/ac_base_monkeypatch.rb
@@ -7,9 +7,8 @@ class ActionCable::Server::Base
         puts "#{Thread.current.object_id} - channel_classes at each"
         config.channel_paths.each { |channel_path| require channel_path }
         puts "#{Thread.current.object_id} - channel_classes past each"
-        puts "#{Thread.current.object_id} - channel_classes at each_with_object"
+        puts "#{Thread.current.object_id} - channel_classes return each_with_object"
         config.channel_class_names.each_with_object({}) { |name, hash| hash[name] = name.constantize }
-        puts "#{Thread.current.object_id} - channel_classes past each_with_object"
       end
     end
   end
